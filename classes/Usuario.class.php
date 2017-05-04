341a4ce6d3e20919281a36fe7dfea0498518f467
Mferreira3 <goodnews.room@hotmail.com>
2017-05-04 16:38:33 -0300

    Adiciona métodos de verificação de dados e criação de conta

--- classes/Usuario.class.php
+++ classes/Usuario.class.php
@@ -2,6 +2,7 @@
 class Usuario extends Record {
 	const TABLE = 'Usuarios'; // table name
 	const PK = 'idUsuario'; // primary key
+
 	/**
 	 * Valida os dados de usuário informados e inicia uma sessão
 	 * @param  	string 		$usuario
@@ -44,6 +45,72 @@ class Usuario extends Record {
 		} else {
 			return "c0"; // Caso algum dado não tenha sido informado
 		}
+	}
+
+	/**
+	 * Realiza o registro de uma nova conta de usuário
+	 * @param  	string 		$usuario
+	 * @param  	string 		$senha
+	 * @param  	string 		$token
+	 * @return 	mixed
+	 */
+	static function registrar($usuario = null, $senha = null, $token = null) {
+		$novoUsuario = new usuario();
+
+		if ($token) { // Registro por token
+			// Pesquisa por algum usuário com o mesmo token cadastrado
+			$usuarioExistente = Usuario::find(array('token = ?', $token), null);
+
+			if (empty($usuarioExistente)) {
+				$novoUsuario->token = $token;
+				$novoUsuario->store();
+
+				return 0;
+			} else {
+				return 'c3';
+			}
+		} elseif (Usuario::dadosInvalidos($usuario, $senha)) {
+			return Usuario::dadosInvalidos($usuario, $senha); // Retorna código de erro caso haja alguma irregularidade nos dados informados
+		} else {
+			$usuarioExistente = Usuario::find(array('usuario = ?', $usuario), null);
+
+			if (empty($usuarioExistente)) {
+				$novoUsuario->usuario = $usuario;
+				$novoUsuario->senha = password_hash($senha, PASSWORD_DEFAULT);
+				$novoUsuario->store();
+
+				return 0;
+			} else {
+				return 'c3';
+			}
+		}
+	}
+
+
+	/**
+	 * Verifica se os detalhes de novo usuário podem ser utilizados
+	 * @param  	string 		$usuario
+	 * @param  	string 		$senha
+	 * @return 	mixed		Código de erro ou 0 para sucesso
+	 */
+	static function dadosInvalidos($usuario = null, $senha = null) {
+		if (empty($usuario)) {
+			return 'c4';
+		}
+
+		if (empty($senha)) {
+			return 'c5';
+		}
+
+		if (strlen($usuario) < 3 || strlen($usuario) > 32) {
+			return 'c6';
+		}
+
+		if (strlen($senha) < 8) {
+			return 'c7';
+		}
+
+		return 0;
 	}
 }
 ?>
