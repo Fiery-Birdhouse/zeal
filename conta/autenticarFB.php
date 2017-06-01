35abb336e7ca7ce89b73e051c422a2e16b8d2f16
Mferreira3 <goodnews.room@hotmail.com>
2017-06-01 19:57:57 -0300

    Adiciona página de autenticação por Facebook

---
+++ conta/autenticarFB.php
@@ -0,0 +1,86 @@
+<?php
+define('INCL_FILE', 'true');
+$def_printHTML = false;
+require_once '../construct.php';
+require '../Facebook/autoload.php';
+
+$helper = $fb->getRedirectLoginHelper();
+
+try {
+	$accessToken = $helper->getAccessToken();
+} catch(Facebook\Exceptions\FacebookResponseException $e) {
+	// When Graph returns an error
+	error_log('Graph returned an error: ' . $e->getMessage());
+	header('location: ' . $def_cred->rootURL);
+	exit;
+} catch(Facebook\Exceptions\FacebookSDKException $e) {
+	// When validation fails or other local issues
+	error_log('Facebook SDK returned an error: ' . $e->getMessage());
+	header('location: ' . $def_cred->rootURL);
+	exit;
+}
+
+if (!isset($accessToken)) {
+	if ($helper->getError()) {
+		if ($helper->getErrorCode() == '200') {
+			header('location: ' . $def_cred->rootURL);
+			exit;
+		}
+
+		header('HTTP/1.0 401 Unauthorized');
+		echo "Error: " . $helper->getError() . "\n";
+		echo "Error Code: " . $helper->getErrorCode() . "\n";
+		echo "Error Reason: " . $helper->getErrorReason() . "\n";
+		echo "Error Description: " . $helper->getErrorDescription() . "\n";
+	} else {
+		header('HTTP/1.0 400 Bad Request');
+		echo 'Bad request';
+	}
+	exit;
+}
+
+// The OAuth 2.0 client handler helps us manage access tokens
+$oAuth2Client = $fb->getOAuth2Client();
+
+// Get the access token metadata from /debug_token
+$tokenMetadata = $oAuth2Client->debugToken($accessToken);
+
+// Validation (these will throw FacebookSDKException's when they fail)
+$tokenMetadata->validateAppId($def_cred->appId);
+
+$tokenMetadata->validateExpiration();
+
+if (!$accessToken->isLongLived()) {
+	// Exchanges a short-lived access token for a long-lived one
+	try {
+		$accessToken = $oAuth2Client->getLongLivedAccessToken($accessToken);
+	} catch (Facebook\Exceptions\FacebookSDKException $e) {
+		error_log("Error getting long-lived access token: " . $helper->getMessage());
+		header('location: ' . $def_cred->rootURL);
+		exit;
+	}
+}
+
+try {
+	// Returns a `Facebook\FacebookResponse` object
+	$response = $fb->get('/me?fields=id,name', $accessToken->getValue());
+} catch(Facebook\Exceptions\FacebookResponseException $e) {
+	error_log('Graph returned an error: ' . $e->getMessage());
+	header('location: ' . $def_cred->rootURL);
+	exit;
+} catch(Facebook\Exceptions\FacebookSDKException $e) {
+	error_log('Facebook SDK returned an error: ' . $e->getMessage());
+	header('location: ' . $def_cred->rootURL);
+	exit;
+}
+
+$user = $response->getGraphUser();
+
+$resultado = Usuario::logar(null, null, $user['id']);
+
+if ($resultado === "c1") {
+	$resultado = Usuario::registrar($user['name'], null, $user['id'], true);
+}
+
+header('location: ' . $def_cred->rootURL);
+?>
