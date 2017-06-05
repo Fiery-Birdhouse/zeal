4c4f5e7ec645f9bb0456430d6571ec60ba0bc6e2
Mferreira3 <goodnews.room@hotmail.com>
2017-06-05 17:32:02 -0300

    Passa a selecionar item do navbar dinamicamente

--- construct.php
+++ construct.php
@@ -13,6 +13,7 @@ if (!isset($_SESSION)) {
 $def_navbar = isset($def_navbar) ? $def_navbar : true; // Define se a barra de navegação deve ser adicionada na construção base da página
 $def_printHTML = isset($def_printHTML) ? $def_printHTML : true; // Define se este arquivo deve imprimir estrutura básica do site
 $def_remainingTime = time() < $def_cred->endTime ? $def_cred->endTime - time() : false; // Tempo restante para finalizar período de registro
+$def_navOption = isset($def_navOption) ? $def_navOption : "home";
 
 if (empty($def_secColorClass)) {
 	$def_secColorClass = date('H') >= 6 && date('H') <= 18 ? 'red' : 'violet';
@@ -54,19 +55,19 @@ if ($def_printHTML) {
 	<body>
 		<?php if ($def_navbar) { ?>
 		<div class="ui vertical inverted sidebar labeled icon menu" id="sidebar">
-			<a class="item navoption" href='<?= $def_cred->rootURL ?>'>
+			<a class="item home" href='<?= $def_cred->rootURL ?>'>
 				<i class="home icon"></i>
 				Início
 			</a>
-			<a class="item">
-				<i class="book icon"></i>
+			<a class="item livros" href="<?= $def_cred->rootURL ?>livros/">
+				<i class="Book icon"></i>
 				Livros
 			</a>
-			<a class="item">
+			<a class="item duvidas">
 				<i class="Help Circle icon"></i>
 				Dúvidas
 			</a>
-			<a class="item">
+			<a class="item conversa">
 				<i class="Comments icon"></i>
 				Conversa
 			</a>
@@ -91,6 +92,8 @@ if ($def_printHTML) {
 					  $("#menulauncher").transition('browse right');
 					}
 				}).sidebar('attach events', '#menulauncher');
+
+				$(".<?= $def_navOption ?>").addClass("navoption");
 				</script>
 				<?php } ?>
 <?php
