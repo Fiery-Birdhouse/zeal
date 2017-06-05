31a8728cbf08fd0d4df2cb0536840a9c2bbe84be
Mferreira3 <goodnews.room@hotmail.com>
2017-06-05 16:18:27 -0300

    Remove argumentos do Facebook após logar por ele

--- construct.php
+++ construct.php
@@ -42,7 +42,13 @@ if ($def_printHTML) {
 		}
 		</style>
 
-
+		<script type="text/javascript">
+			if (window.location.hash && window.location.hash == '#_=_') {
+				if (window.history && history.pushState) {
+					window.history.pushState("", document.title, window.location.pathname);
+				}
+			}
+		</script>
 	</head>
 
 	<body>
