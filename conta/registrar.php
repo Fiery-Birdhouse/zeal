b555f70f066bc74c35223ca835d2c20611fc9574
Mferreira3 <goodnews.room@hotmail.com>
2017-06-05 16:19:44 -0300

    Adiciona restrições ao registro

--- conta/registrar.php
+++ conta/registrar.php
@@ -4,6 +4,12 @@ $def_printHTML = false;
 require_once '../helper/ajaxRestriction.php';
 require_once '../construct.php';
 
-$resultado = Usuario::registrar($_POST['usuario'], $_POST['senha'], null, true);
+// Verifica se a criação de novas contas está ativada
+if ($def_remainingTime) {
+	$resultado = Usuario::registrar($_POST['usuario'], $_POST['senha'], null, true);
+} else {
+	$resultado = "c8";
+}
+
 echo $resultado ? notificacoes($resultado) : 0; // Imprime mensagem de erro caso tenha ocorrido alguma falha
 ?>
