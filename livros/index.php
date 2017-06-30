89cd8f55fa18b53eca3e88daa163829e5ef5b6fc
MFerreira3 <goodnews.room@hotmail.com>
2017-06-30 19:41:20 -0300

    Esboço da página principal da seção de livros

---
+++ livros/index.php
@@ -0,0 +1,155 @@
+<?php
+define('INCL_FILE', 'true');
+$def_navOption = "livros";
+require_once "../construct.php";
+?>
+
+<div style="height: 7rem; width: auto;"></div>
+
+<div class="ui four stackable link cards">
+	<div class="card red" href="#">
+		<div class="content">
+			<div class="header">Cute Dog</div>
+			<div class="description">
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras neque leo, pulvinar a consequat quis, porttitor ut tellus. Donec libero libero, iaculis sit amet purus sed, vehicula blandit erat. Mauris viverra convallis interdum. Nulla lobortis nunc vel sed. </p>
+			</div>
+		</div>
+		<div class="extra content">
+			<span class="left floated like">
+				<i class="like icon"></i>
+				Curtir
+			</span>
+			<div class="right floated author"id="cardUser">
+				<img class="ui avatar image" src="https://semantic-ui.com/images/avatar/small/matt.jpg"> Mattheus com dois Ts cause why not?
+			</div>
+		</div>
+	</div>
+
+	<div class="card red" href="#">
+		<div class="content">
+			<div class="header">Cute Dog</div>
+			<div class="description">
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras neque leo, pulvinar a consequat quis, porttitor ut tellus. Donec libero libero, iaculis sit amet purus sed, vehicula blandit erat. Mauris viverra convallis interdum. Nulla lobortis nunc vel sed. </p>
+			</div>
+		</div>
+		<div class="extra content">
+			<span class="left floated like">
+				<i class="like icon"></i>
+				Curtir
+			</span>
+			<div class="right floated author"id="cardUser">
+				<img class="ui avatar image" src="https://semantic-ui.com/images/avatar/small/matt.jpg"> Mattheus com dois Ts cause why not?
+			</div>
+		</div>
+	</div>
+
+	<div class="card red" href="#">
+		<div class="content">
+			<div class="header">Cute Dog</div>
+			<div class="description">
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras neque leo, pulvinar a consequat quis, porttitor ut tellus. Donec libero libero, iaculis sit amet purus sed, vehicula blandit erat. Mauris viverra convallis interdum. Nulla lobortis nunc vel sed. </p>
+			</div>
+		</div>
+		<div class="extra content">
+			<span class="left floated like">
+				<i class="like icon"></i>
+				Curtir
+			</span>
+			<div class="right floated author"id="cardUser">
+				<img class="ui avatar image" src="https://semantic-ui.com/images/avatar/small/matt.jpg"> Mattheus com dois Ts cause why not?
+			</div>
+		</div>
+	</div>
+
+	<div class="card red" href="#">
+		<div class="content">
+			<div class="header">Cute Dog</div>
+			<div class="description">
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras neque leo, pulvinar a consequat quis, porttitor ut tellus. Donec libero libero, iaculis sit amet purus sed, vehicula blandit erat. Mauris viverra convallis interdum. Nulla lobortis nunc vel sed. </p>
+			</div>
+		</div>
+		<div class="extra content">
+			<span class="left floated like">
+				<i class="like icon"></i>
+				Curtir
+			</span>
+			<div class="right floated author"id="cardUser">
+				<img class="ui avatar image" src="https://semantic-ui.com/images/avatar/small/matt.jpg"> Mattheus com dois Ts cause why not?
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="ui four stackable link cards">
+	<div class="card red" href="#">
+		<div class="content">
+			<div class="header">Cute Dog</div>
+			<div class="description">
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras neque leo, pulvinar a consequat quis, porttitor ut tellus. Donec libero libero, iaculis sit amet purus sed, vehicula blandit erat. Mauris viverra convallis interdum. Nulla lobortis nunc vel sed. </p>
+			</div>
+		</div>
+		<div class="extra content">
+			<span class="left floated like">
+				<i class="like icon"></i>
+				Curtir
+			</span>
+			<div class="right floated author"id="cardUser">
+				<img class="ui avatar image" src="https://semantic-ui.com/images/avatar/small/matt.jpg"> Mattheus com dois Ts cause why not?
+			</div>
+		</div>
+	</div>
+
+	<div class="card red" href="#">
+		<div class="content">
+			<div class="header">Cute Dog</div>
+			<div class="description">
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras neque leo, pulvinar a consequat quis, porttitor ut tellus. Donec libero libero, iaculis sit amet purus sed, vehicula blandit erat. Mauris viverra convallis interdum. Nulla lobortis nunc vel sed. </p>
+			</div>
+		</div>
+		<div class="extra content">
+			<span class="left floated like">
+				<i class="like icon"></i>
+				Curtir
+			</span>
+			<div class="right floated author"id="cardUser">
+				<img class="ui avatar image" src="https://semantic-ui.com/images/avatar/small/matt.jpg"> Mattheus com dois Ts cause why not?
+			</div>
+		</div>
+	</div>
+
+	<div class="card red" href="#">
+		<div class="content">
+			<div class="header">Cute Dog</div>
+			<div class="description">
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras neque leo, pulvinar a consequat quis, porttitor ut tellus. Donec libero libero, iaculis sit amet purus sed, vehicula blandit erat. Mauris viverra convallis interdum. Nulla lobortis nunc vel sed. </p>
+			</div>
+		</div>
+		<div class="extra content">
+			<span class="left floated like">
+				<i class="like icon"></i>
+				Curtir
+			</span>
+			<div class="right floated author"id="cardUser">
+				<img class="ui avatar image" src="https://semantic-ui.com/images/avatar/small/matt.jpg"> Mattheus com dois Ts cause why not?
+			</div>
+		</div>
+	</div>
+
+	<div class="card red" href="#">
+		<div class="content">
+			<div class="header">Cute Dog</div>
+			<div class="description">
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras neque leo, pulvinar a consequat quis, porttitor ut tellus. Donec libero libero, iaculis sit amet purus sed, vehicula blandit erat. Mauris viverra convallis interdum. Nulla lobortis nunc vel sed. </p>
+			</div>
+		</div>
+		<div class="extra content">
+			<span class="left floated like">
+				<i class="like icon"></i>
+				Curtir
+			</span>
+			<div class="right floated author"id="cardUser">
+				<img class="ui avatar image" src="https://semantic-ui.com/images/avatar/small/matt.jpg"> Mattheus com dois Ts cause why not?
+			</div>
+		</div>
+	</div>
+</div>
